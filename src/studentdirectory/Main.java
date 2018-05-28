209d6a7cb4fb7b51d03a5040bd40646332a81230
aaron001ambas <37833096+aaron001ambas@users.noreply.github.com>
2018-05-28 17:21:20 +0800

    Transferred the fillDirectory function to the Directory Class rather than the Main class

--- src/studentdirectory/Main.java
+++ src/studentdirectory/Main.java
@@ -9,8 +9,9 @@ public class Main {
 			students[i] = new Student();
 		}
 		
-		fillDirectory(students);
+		Directory.fillDirectory(students);
 		
+		// Change these values to get different outputs 
 		String firstName = "John";
 		String lastName = "Nguyen";
 		String studentID = "201701";
@@ -26,50 +27,4 @@ public class Main {
 		Directory.listStudents(i, students, lastName); // Lists the students with the specified last Name.
 		
 	}
-	
-	public static void fillDirectory(Student[] students) {
-		
-		students[0].setFirstName("John");
-		students[0].setLastName("Smith");
-		students[0].setCourse("IT");
-		students[0].setYear("1st Year");
-		students[0].setStudentID("201801");
-		students[0].setAge(18);
-
-		students[1].setFirstName("Mark");
-		students[1].setLastName("Wallace");
-		students[1].setCourse("IT");
-		students[1].setYear("1st Year");
-		students[1].setStudentID("201802");
-		students[1].setAge(18);
-
-		students[2].setFirstName("Tony");
-		students[2].setLastName("Reese");
-		students[2].setCourse("MA");
-		students[2].setYear("2nd Year");
-		students[2].setStudentID("201701");
-		students[2].setAge(19);
-		
-		students[3].setFirstName("Liza");
-		students[3].setLastName("Johnson");
-		students[3].setCourse("BA");
-		students[3].setYear("1st Year");
-		students[3].setStudentID("201804");
-		students[3].setAge(18);
-		
-		students[4].setFirstName("Kim");
-		students[4].setLastName("Nguyen");
-		students[4].setCourse("ECE");
-		students[4].setYear("1st Year");
-		students[4].setStudentID("201805");
-		students[4].setAge(17);
-		
-		students[5].setFirstName("Keichi");
-		students[5].setLastName("Takayama");
-		students[5].setCourse("ECE");
-		students[5].setYear("2nd Year");
-		students[5].setStudentID("201702");
-		students[5].setAge(17);
-	}
-
 }
